45c1af26ed3c0dfdef88809e3bac0e155f2eaec0
Marvin Joseph <202040175@psu.palawan.edu.ph>
2026-04-14 01:20:41 +0800

    remove forms on createform.vue except the site details section

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ Route::inertia('/', 'Login/Index')->name('login');
 Route::inertia('/dashboard', 'Dashboard/Index')->name('dashboard');
 Route::inertia('/customers', 'Customers/Index')->name('customers');
 Route::inertia('/asbestos-audits', 'AsbestosAudits/Index')->name('asbestosaudits');
-Route::inertia('/asbestos-audits/create', 'AsbestosAudits/AsbestosForm')->name('asbestosaudits.asbestosform');
-Route::inertia('/asbestos-audits/edit', 'AsbestosAudits/AsbestosForm')->name('asbestosaudits.asbestosform');
+Route::inertia('/asbestos-audits/create', 'AsbestosAudits/CreateForm')->name('asbestosaudits.createform');
+Route::inertia('/asbestos-audits/edit', 'AsbestosAudits/EditForm')->name('asbestosaudits.editform');
